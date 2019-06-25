de5069972fa2bdd820ad35f672c68957df332246
Manoj Pissay A <manojpissay@gmail.com>
2019-06-26 00:15:01 +0530

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDQka_e93o9zxmEMVRRyDdT_L0bLrRCl1s",
-    authDomain: "my-messenger-b289e.firebaseapp.com",
-    databaseURL: "https://my-messenger-b289e.firebaseio.com",
-    projectId: "my-messenger-b289e",
-    storageBucket: "my-messenger-b289e.appspot.com",
-    messagingSenderId: "544864624089",
-    appId: "1:544864624089:web:5918add90e68e7b0"
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " "
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
